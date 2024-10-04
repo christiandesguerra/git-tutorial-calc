42936c72e842855641d9909a55b0306d8e118bff
christiandesguerra <christiandesguerra@gmail.com>
2024-10-05 01:13:03 +0800

    fix add

--- src/main/java/com/example/git_tutorial/controller/CalculatorController.java
+++ src/main/java/com/example/git_tutorial/controller/CalculatorController.java
@@ -3,6 +3,7 @@ package com.example.git_tutorial.controller;
 import com.example.git_tutorial.service.CalculatorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,8 @@ public class CalculatorController {
     private final CalculatorService calculatorService;
 
     @GetMapping("/add/{firstNum}/{secondNum}")
-    public Integer add(Integer firstNum, Integer secondNum) {
+    public Integer add(@PathVariable Integer firstNum,
+                       @PathVariable Integer secondNum) {
         return calculatorService.add(firstNum, secondNum);
     }
 }
