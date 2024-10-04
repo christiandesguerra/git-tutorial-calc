7ca9bde85b8157ae4253bab5a99f61c6609f7980
christiandesguerra <christiandesguerra@gmail.com>
2024-10-04 23:10:50 +0800

    initial commit

---
+++ src/main/java/com/example/git_tutorial/GitTutorialCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTutorialCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTutorialCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_tutorial/GitTutorialCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTutorialCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
